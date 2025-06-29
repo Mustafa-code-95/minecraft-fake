d86e869c86270a6d8d204460d5d44e547689e5ac
Mustafa <mustafa2014aksoy@gmail.com>
2025-06-29 09:50:56 +0200

    Create minecraft_crash.py

---
+++ minecraft_crash.py
@@ -0,0 +1,90 @@
+from ursina import Sky
+from ursina import Ursina
+from ursina import Button
+from ursina import color
+from ursina import Text
+from ursina import held_keys
+from ursina import destroy
+from ursina import scene
+from ursina import mouse
+from ursina.prefabs.first_person_controller import FirstPersonController
+from ursina import application
+
+app = Ursina()
+player = FirstPersonController()
+player.position = 2, 20, 2
+Sky(texture='sky_sunset')
+list = ['der.jpeg', 'retro.png', 'cry.png', 'obs.png', 'glo.jpg', 'bri.jpg', 'red.jpg', 'ingo.png', 'stein1.png', 'stein2.png', 'oo.png', '44.png', 'rt.png', 'buer.jpg', 'ft.png', 'rt.jpg', 'tatata.jpg', 'ste.jpg', 'derr.jpg']
+list_draw = ["dirt", "Retro Obsidian", "Crying Obsidian", "Obsidian", "glow stone", "Blackstone", "Redstone", "silver erz", "Stone", "Kobbel Stone", "Nubered hotbar", "Clear Glas", "Glas", "Zigel", "end stone", "wood", "Nether stone", "snow", "Sand"]
+d = 0
+t = 0
+cv = [color.white, color.white66]
+wer = []
+a = list[d]
+ddd = list_draw[d]
+boxes = []
+g = 1
+b_b_text = Text('', origin=(0,7), scale=2)
+
+
+def add_box(position, color, texture):
+    boxes.append(
+        Button(
+            parent=scene,
+            model='cube',
+            origin=0.5,
+            color=color,
+            position=position,
+            texture=texture
+        )
+    )
+
+
+for ioo in range(2):
+    g = ioo + 1
+    for ou in range(51):
+        x = ou + 1
+        for hu in range(51):
+            y = hu + 1
+            add_box( (x, g, y), color=color.white, texture='ste.jpg')
+
+
+def update():
+    global d, ddd, list_draw
+    if held_keys['c']:
+        application.quit()
+    if held_keys['m']:
+        player.position = 0, 17, 0
+    if ((held_keys['l']) or (held_keys['r'])):
+        ddd = list_draw[d]
+        b_b_text.text = ddd
+
+
+def input(key):
+    global a, d, ddd, cv, t
+    for box in boxes:
+        if box.hovered:
+            if key == "right mouse down":
+                if a == 'tr1.png':
+                    t = 1
+                else:
+                    t = 0
+                add_box(box.position + mouse.normal, color=cv[t], texture=a)
+            if key == "left mouse down":
+                boxes.remove(box)
+                destroy(box)
+    if key == "l":
+        if d == 0:
+            d = len(list)-1
+        else:
+            d -= 1
+        a = list[d]
+    if key == "r":
+        if d == len(list)-1:
+            d = 0
+        else:
+            d += 1
+        a = list[d]
+
+
+app.run()
